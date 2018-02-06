98f3f0e81d667ddbc8801781714a665e14de9347
andreasma <maand@gmx.de>
2018-02-06 10:47:48 +0100

    added a test for manifest.xml

--- oxtTest.py
+++ oxtTest.py
@@ -9,12 +9,26 @@ try:
     contents=f.read()
     if contents.count('http://libreoffice.org/extensions/description/2011') >0:
         print('LibreOffice extension declaration included')
-    elif content.count('http://openoffice.org/extensions/description/2006') >0:
+    elif contents.count('http://openoffice.org/extensions/description/2006') >0:
         print('OpenOffice extension declaration included')
     else:
         print('There is a xml extension declarartion missing in the description.xml.')
     f.close
 except (KeyError):
-    print ('There is a file description.xml missing in the extension. The extension is not valid!')
+    print ('There is a file description.xml missing in the extension. Thus the extension is not valid!')
+    
+try:
+    g=zip.open('META-INF/manifest.xml')
+    contents=g.read()
+    if contents.count('<manifest:manifest xmlns:manifest="http://openoffice.org/2001/manifest">') > 0:
+        pass
+    else:
+        print('There is a xml declaration missing in the file manifest.xml starting with the following xml-tag: \n'
+        '<manifest:manifest xmlns:manifest="http://openoffice.org/2001/manifest">.')
+    g.close
+    
+except (KeyError):
+    print ('There is a file manifest.xml missing in the extension. Please add such file to the '
+           'extension to get it valid!')
 
 
