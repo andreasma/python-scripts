2307858d0121aea2a5725627a1f878f8920b7755
andreasma <maand@gmx.de>
2018-09-17 18:03:20 +0200

    Add queries for included xml-elements and show information about the findings to the user.

--- oxtTest2.py
+++ oxtTest2.py
@@ -1,6 +1,7 @@
 import zipfile
 import errno
 import xml.etree.ElementTree as ET
+import re
 
 extensionfile= raw_input('Please type in the name of the extension file you want to investigate: ')
 
@@ -17,6 +18,49 @@ try:
         print ('LibreOffice extension declaration inclueded')
     else:
         ('There is a xml extension declarartion missing in the description.xml.')
+
+    childtags = []
+    for child in root:
+        childtags.append(child.tag)
+
+    z = ''
+    for element in childtags:
+        version = re.findall(r'version', element)
+        registration = re.findall(r'registration', element)
+        identifier = re.findall(r'identifier', element)
+        icon = re.findall(r'icon', element)
+        display= re.findall(r'display-name', element)
+        description = re.findall(r'extension-description', element)
+        dependencies = re.findall(r'dependencies', element)
+        publisher = re.findall(r'publisher', element)
+
+    if version.count > 0:
+        print ('Extension version information included.')
+    else:
+        print ('There is an Extension version information missing.')
+
+    if registration.count > 0:
+        print ('Information about license included.')
+    else:
+        print ('There is a link to the license information missing. \nPlease add this link and make sure that it'
+               'points to the license file inside the Extension.')
+
+    if identifier.count > 0:
+        print ('The identifier xml-tag is included.')
+    else:
+        print ('There is the identifier xml-tag missing. ')
+
+    if icon.count > 0:
+        print ('The icon xml-tag is included.')
+    else:
+        print ('The icon xml-tag with a link to an icon for the Extension is missing.')
+
+    if dependencies.count > 0:
+        print ('Information about program version included.')
+    else:
+        print ('There is no information about the (minimal / maximal program version included')
+
+
 
 
     f.close
